0c9c88dc92f974829135ca780ff63d5e1de780ba
talmeym <mtalmey@gmail.com>
2024-01-10 12:00:34 +0000

    emarte uk

--- src/main/java/uk/emarte/regurgitator/core/IndexOfProcessorYmlLoader.java
+++ src/main/java/uk/emarte/regurgitator/core/IndexOfProcessorYmlLoader.java
@@ -6,8 +6,7 @@ package uk.emarte.regurgitator.core;
 
 import java.util.Set;
 
-import static uk.emarte.regurgitator.core.CoreConfigConstants.SOURCE;
-import static uk.emarte.regurgitator.core.CoreConfigConstants.VALUE;
+import static uk.emarte.regurgitator.core.CoreConfigConstants.*;
 import static uk.emarte.regurgitator.core.Log.getLog;
 import static uk.emarte.regurgitator.core.YmlConfigUtil.loadOptionalBool;
 import static uk.emarte.regurgitator.core.YmlConfigUtil.loadOptionalStr;
@@ -19,7 +18,7 @@ public class IndexOfProcessorYmlLoader implements YmlLoader<IndexOfProcessor> {
     public IndexOfProcessor load(Yaml yaml, Set<Object> allIds) throws RegurgitatorException {
         String source = loadOptionalStr(yaml, SOURCE);
         String value = loadOptionalStr(yaml, VALUE);
-        boolean backwards = loadOptionalBool(yaml, CoreConfigConstants.LAST);
+        boolean backwards = loadOptionalBool(yaml, LAST);
         log.debug("Loaded index of processor");
         return new IndexOfProcessor(new ValueSource(source != null ? new ContextLocation(source) : null, value), backwards);
     }
